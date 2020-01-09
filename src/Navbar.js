c76c73e19fa0a919a6014247521be0ae4378385f
Grzegorz Konarski <gk@pubfront.com>
2020-01-09 14:11:09 +0100

    Create LanguageContext

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,23 +11,41 @@ import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/NavbarStyles';
 
 import { ThemeContext } from './contexts/ThemeContext';
+import { withLanguageContext } from './contexts/LanguageContext';
+
+const content = {
+  english: {
+    search: "Search",
+    flag: "🇬🇧"
+  },
+  french: {
+    search: "Chercher",
+    flag: "🇫🇷"
+  },
+  spanish: {
+    search: "Buscar",
+    flag: "🇪🇸"
+  }
+};
+
 class Navbar extends Component {
+  static contextType = ThemeContext;
   render() {
     const { isDarkMode, toggleTheme } = this.context; 
     const { classes } = this.props;
+    const { language } = this.props.languageContext;
+    const { search, flag } = content[language];
 
     return (
       <div className={classes.root}>
-        <AppBar position="static" color="primary">
         <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
           <ToolBar>
             <IconButton className={classes.menuButton}>
-              <span>🇫🇷</span>
+              <span>{ flag }</span>
             </IconButton>
             <Typography className={classes.title} variant="h6" color="inherit">
               App title
             </Typography>
-            <Switch />
             <Switch onChange={ toggleTheme } />
             <div className={classes.grow} />
             <div className={classes.search}>
@@ -35,7 +53,7 @@ class Navbar extends Component {
                 <SearchIcon />
               </div>
               <InputBase 
-                placeholder="Search..." 
+                placeholder={`${ search }...`} 
                 classes={{
                 root: classes.inputRoot,
                 input: classes.inputInput
@@ -49,4 +67,4 @@ class Navbar extends Component {
 }
 
 
-export default withStyles(styles)(Navbar);
+export default withLanguageContext(withStyles(styles)(Navbar));
