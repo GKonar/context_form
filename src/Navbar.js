6467f75b0d6d6c3afa3129ac4de1d13b4132d690
Grzegorz Konarski <gk@pubfront.com>
2020-01-10 12:21:10 +0100

    Refactor to use Hooks

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/ToolBar';
 import IconButton from '@material-ui/core/IconButton';
@@ -11,7 +11,7 @@ import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/NavbarStyles';
 
 import { ThemeContext } from './contexts/ThemeContext';
-import { withLanguageContext } from './contexts/LanguageContext';
+import { LanguageContext } from './contexts/LanguageContext';
 
 const content = {
   english: {
@@ -28,43 +28,39 @@ const content = {
   }
 };
 
-class Navbar extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isDarkMode, toggleTheme } = this.context; 
-    const { classes } = this.props;
-    const { language } = this.props.languageContext;
-    const { search, flag } = content[language];
+function Navbar(props) {
+  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+  const { language } = useContext(LanguageContext);
+  const { classes } = props;
+  const { search, flag } = content[language];
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
-          <ToolBar>
-            <IconButton className={classes.menuButton}>
-              <span>{ flag }</span>
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit">
-              App title
-            </Typography>
-            <Switch onChange={ toggleTheme } />
-            <div className={classes.grow} />
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase 
-                placeholder={`${ search }...`} 
-                classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput
-              }}/>
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
+        <ToolBar>
+          <IconButton className={classes.menuButton}>
+            <span>{ flag }</span>
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit">
+            App title
+          </Typography>
+          <Switch onChange={ toggleTheme } />
+          <div className={classes.grow} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-          </ToolBar>
-        </AppBar>
-      </div>
-    )
-  }
+            <InputBase 
+              placeholder={`${ search }...`} 
+              classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput
+            }}/>
+          </div>
+        </ToolBar>
+      </AppBar>
+    </div>
+  )
 }
 
-
-export default withLanguageContext(withStyles(styles)(Navbar));
+export default withStyles(styles)(Navbar);
